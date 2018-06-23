dd889ce814568ac011d8791325aabcabf2a25dd4
caozhiyuan <caozhiyuan@suiyi.com.cn>
2018-06-23 12:52:31 +0800

    test

--- src/test/java/com/example/demo/utils/HttpClientManagerTest.java
+++ src/test/java/com/example/demo/utils/HttpClientManagerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -49,4 +49,33 @@ public class HttpClientManagerTest {
         }
     }
 
+
+    @Test
+    public void TestHttp2() throws Exception {
+
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(200);
+        while (true) {
+
+            long startTime = System.currentTimeMillis();   //获取开始时间
+
+            int count = 10000;
+            CountDownLatch countDownLatch = new CountDownLatch(count);
+            for (int i = 0; i < count; i++) {
+                fixedThreadPool.execute(() -> {
+                    try {
+                        String str = httpClientManager.getAsync("http://10.1.62.66:5500/plaintext");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        countDownLatch.countDown();
+                    }
+                });
+            }
+
+            countDownLatch.await();
+            long endTime = System.currentTimeMillis(); //获取结束时间
+            System.out.println("程序运行时间： " + (endTime - startTime) + "ms");
+            Thread.sleep(1000);
+        }
+    }
 }
