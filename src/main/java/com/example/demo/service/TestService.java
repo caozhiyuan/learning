f3b98013ee72f4012197bcac53f21efbf1a56d4a
caozhiyuan <caozhiyuan@suiyi.com.cn>
2018-06-26 13:54:47 +0800

    test

--- src/main/java/com/example/demo/service/TestService.java
+++ src/main/java/com/example/demo/service/TestService.java
@@ -34,7 +34,7 @@ public class TestService {
     @Async
     public CompletableFuture<Boolean> sendMail() throws InterruptedException {
         CompletableFuture ts = new CompletableFuture();
-        Thread.sleep(50);
+        Thread.sleep(5);
         ts.complete(true);
         return ts;
     }
